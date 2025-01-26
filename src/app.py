bad15429a464c2d09d46ec7625349d09b5fe70c9
peakit <hmanak@gmail.com>
2025-01-26 20:16:19 +0530

    testing

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 import subprocess
 import shlex
+import os
 
 def handler(event, context):
-    gcloud_init_cmd = "cat ${GOOGLE_APPLICATION_CREDENTIALS}"
-    # gcloud_init_cmd = "gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}"
-    cbt_import_cmd = "cbt import test123 test.csv column-family=info batch-size=50000 workers=10 timestamp=now"
+    credentials_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    gcloud_init_cmd = f"cat {credentials_path}"
     subprocess.run(shlex.split(gcloud_init_cmd), check=True)
+    # gcloud_init_cmd = "gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}"
+
+    cbt_import_cmd = "cbt import test123 test.csv column-family=info batch-size=50000 workers=10 timestamp=now"
     subprocess.run(shlex.split(cbt_import_cmd), check=True)
     return {
         'statusCode': 200,
