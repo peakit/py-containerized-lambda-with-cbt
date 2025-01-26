334bf1aeb499791d3a601089b40726693ec22864
peakit <hmanak@gmail.com>
2025-01-26 20:06:07 +0530

    dummy sa to test the working

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ import subprocess
 import shlex
 
 def handler(event, context):
-    gcloud_init_cmd = "gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}"
+    gcloud_init_cmd = "cat ${GOOGLE_APPLICATION_CREDENTIALS}"
+    # gcloud_init_cmd = "gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}"
     cbt_import_cmd = "cbt import test123 test.csv column-family=info batch-size=50000 workers=10 timestamp=now"
     subprocess.run(shlex.split(gcloud_init_cmd), check=True)
     subprocess.run(shlex.split(cbt_import_cmd), check=True)
