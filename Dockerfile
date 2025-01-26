1d3c801eb5bdbe5a99f83dfd3ac2b05aad4f836d
peakit <hmanak@gmail.com>
2025-01-26 19:45:10 +0530

    mocking gcloud and cbt commands along with sa

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM public.ecr.aws/lambda/python:3.11
 COPY requirements.txt ${LAMBDA_TASK_ROOT}
 RUN pip3 install --user -r requirements.txt
-COPY src/app.py ${LAMBDA_TASK_ROOT}
+COPY src/ ${LAMBDA_TASK_ROOT}
+ENV GOOGLE_APPLICATION_CREDENTIALS=${LAMBDA_TASK_ROOT}/service-account-key.json
+COPY .cbtrc /tmp
 RUN yum install -y tar gzip unzip && \
     yum clean all
 RUN mkdir -p /opt
--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
+import subprocess
+import shlex
+
 def handler(event, context):
+    gcloud_init_cmd = "gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS"
+    cbt_import_cmd = "cbt import test123 test.csv column-family=info batch-size=50000 workers=10 timestamp=now"
+    subprocess.run(shlex.split(gcloud_init_cmd), check=True)
+    subprocess.run(shlex.split(cbt_import_cmd), check=True)
     return {
         'statusCode': 200,
         'body': 'Hello World!'
